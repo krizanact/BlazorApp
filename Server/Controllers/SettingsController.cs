da719cf59344d6f1fdd3833814b5c9e16ebddd67
krizanact <krizanactoni691@gmail.com>
2024-10-15 15:54:35 +0200

    another

--- Server/Controllers/SettingsController.cs
+++ Server/Controllers/SettingsController.cs
@@ -9,6 +9,6 @@ public class SettingsController : ControllerBase
     [HttpGet("base-url")]
     public ActionResult GetBaseUrl(CancellationToken cancellationToken)
     {
-        return Ok($"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}/{HttpContext.Request.Host.Port}xaxa");
+        return Ok($"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}/{HttpContext.Request.Host.Port}xa");
     }
 }
